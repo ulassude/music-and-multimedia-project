b538db947730ff8b8b299ffb1defbb1cb0ee635f
AliSinaDevelo <alisinakarimi.2003@gmail.com>
2025-05-10 21:41:09 +0200

    settings config

--- media_project/media_project/settings.py
+++ media_project/media_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,20 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "stream_app"
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
